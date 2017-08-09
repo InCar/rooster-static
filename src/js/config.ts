a5acaff9c8428d77fdf63bf44bf55ab52ec03b9a
AceBear <AceBear@users.noreply.github.com>
2017-08-09 10:01:42 -0500

    cdn (#2)

--- src/js/config.ts
+++ src/js/config.ts
@@ -46,22 +46,22 @@ class Config{
         let pathConfig: any;
         if (this._debug) {
             pathConfig = {
-                text: ["//cdn.jsdelivr.net/requirejs.text/2.0.12/text", "/lib/requirejs-text/text"],
-                jquery: ["//cdn.jsdelivr.net/jquery/3.2.1/jquery", "/lib/jquery/dist/jquery"],
-                cookie: ["//cdn.jsdelivr.net/jquery.cookie/1.4.1/jquery.cookie", "/lib/jquery.cookie/jquery.cookie"],
-                bootstrap: ["//cdn.jsdelivr.net/bootstrap/3.3.7/js/bootstrap", "/lib/bootstrap/dist/js/bootstrap"],
-                bluebird: ["//cdn.jsdelivr.net/bluebird/3.5.0/bluebird", "/lib/bluebird/js/browser/bluebird"],
-                vue: ["//cdn.jsdelivr.net/vue/2.3.2/vue", "/lib/vue/dist/vue"]
+                text: ["//cdn.incarcloud.com/lib/requirejs.text/2.0.15/text", "/lib/requirejs-text/text"],
+                jquery: ["//cdn.incarcloud.com/lib/jquery/3.2.1/jquery", "/lib/jquery/dist/jquery"],
+                cookie: ["//cdn.incarcloud.com/lib/jquery.cookie/1.4.29/jquery.cookie", "/lib/jquery.cookie/jquery.cookie"],
+                bootstrap: ["//cdn.incarcloud.com/lib/bootstrap/3.3.7/js/bootstrap", "/lib/bootstrap/dist/js/bootstrap"],
+                bluebird: ["//cdn.incarcloud.com/lib/bluebird/3.5.0/bluebird", "/lib/bluebird/js/browser/bluebird"],
+                vue: ["//cdn.incarcloud.com/lib/vue/2.4.2/vue", "/lib/vue/dist/vue"]
             };
         }
         else {
             pathConfig = {
-                text: ["//cdn.jsdelivr.net/requirejs.text/2.0.12/text.min", "/lib/requirejs-text/text.min"],
-                jquery: ["//cdn.jsdelivr.net/jquery/3.2.1/jquery.min", "/lib/jquery/dist/jquery.min"],
-                cookie: ["//cdn.jsdelivr.net/jquery.cookie/1.4.1/jquery.cookie.min", "/lib/jquery.cookie/jquery.cookie.min"],
-                bootstrap: ["//cdn.jsdelivr.net/bootstrap/3.3.7/js/bootstrap.min", "/lib/bootstrap/dist/js/bootstrap/bootstrap.min"],
-                bluebird: ["//cdn.jsdelivr.net/bluebird/3.5.0/bluebird.min", "/lib/bluebird/js/browser/bluebird.min"],
-                vue: ["//cdn.jsdelivr.net/vue/2.3.2/vue.min", "/lib/vue/dist/vue.min"]
+                text: ["//cdn.incarcloud.com/lib/requirejs.text/2.0.15/text.min", "/lib/requirejs-text/text.min"],
+                jquery: ["//cdn.incarcloud.com/lib/jquery/3.2.1/jquery.min", "/lib/jquery/dist/jquery.min"],
+                cookie: ["//cdn.incarcloud.com/lib/jquery.cookie/1.4.29/jquery.cookie.min", "/lib/jquery.cookie/jquery.cookie.min"],
+                bootstrap: ["//cdn.incarcloud.com/lib/bootstrap/3.3.7/js/bootstrap.min", "/lib/bootstrap/dist/js/bootstrap/bootstrap.min"],
+                bluebird: ["//cdn.incarcloud.com/lib/bluebird/3.5.0/bluebird.min", "/lib/bluebird/js/browser/bluebird.min"],
+                vue: ["//cdn.incarcloud.com/lib/vue/2.4.2/vue.min", "/lib/vue/dist/vue.min"]
             };
         }
         pathConfig['modernizr'] = "/lib/modernizr";
