d7438db5e7592f4ef85b7031a75f7c74f212cb58
AceBear <AceBear@users.noreply.github.com>
2017-08-24 02:54:37 -0500

    支付宝登录支持 (#3)

---
+++ src/js/3rd/3rd.ts
@@ -0,0 +1,27 @@
+﻿import * as Vue from "vue";
+import { App } from "../app";
+
+export = class Home {
+    constructor() {
+    }
+
+    public init(resolve, reject) {
+        requirejs(['text!3rd/3rd.html'], (template) => {
+
+            resolve({
+                name: "Third",
+                template,
+                props: ["stack"],
+                mounted: function () {
+                    if (this.stack && this.stack.length > 0) {
+                        if (this.stack[0] == "alipay") {
+                            // TODO: 支付宝登录
+                            console.info(App.getQuery("auth_code", window.location.href));
+                            console.info(App.getCfg());
+                        }
+                    }
+                }
+            });
+        });
+    }
+};
---
+++ src/js/app-dev-sample.ts
@@ -0,0 +1,10 @@
+﻿// 开发人员复制此文件为"app-dev.ts"，在其中设置自己配置项
+
+define([], () => {
+    return {
+        // 后台API位置
+        "api": "http://localhost:8080",
+        // 支付宝登录沙箱 帐号qfdixk1030@sandbox.com 密码111111
+        "alipay": "https://openauth.alipaydev.com/oauth2/publicAppAuthorize.htm?app_id=2016081600256157&scope=auth_user&redirect_uri=http%3a%2f%2flocalhost:8080/3rd/alipay&state=sandbox"
+    };
+});
